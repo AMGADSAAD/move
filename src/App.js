427e29725bfd82b7118cd0dcb637894e64be0063
AMGADSAAD <amgadsaad905@gmail.com>
2024-10-09 11:26:35 +0300

    react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "../src/Routes/Home.jsx";
+import Movie from "./Routes/Movie.jsx";
+import Contact from "./Routes/Contact.jsx";
+
+// api
+/* https://www.omdbapi.com/?s=adventure&apikey=42ef2173 */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movie" element={<Movie />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
